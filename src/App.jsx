e753a9688a258a726e50833a04791c8e6a7c379b
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-08-31 11:58:03 +0800

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="logo">
+            <a href="#" className="text-decoration-none text-light">
+              QuizTwist
+            </a>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default App;
