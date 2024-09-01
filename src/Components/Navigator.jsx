763bfc9fbbbdd47f8db0e44550202a56c66673bc
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-09-01 10:20:53 +0800

    added modal login and register

--- src/Components/Navigator.jsx
+++ src/Components/Navigator.jsx
@@ -12,29 +12,212 @@ const Navigator = () => {
   };
 
   return (
-    <nav className={`container ${styles["nav-parent"]}`}>
-      <div
-        className={`container d-flex justify-content-between align-items-center ${styles["nav-container"]}`}
-      >
-        <Logo />
-
-        {/* Dropdown menu */}
-        <ul
-          className={`d-flex list-unstyled gap-2 ${styles["nav-link-parent"]} ${styles["show"]}`}
+    <>
+      <nav className={`container ${styles["nav-parent"]}`}>
+        <div
+          className={`container d-flex justify-content-between align-items-center ${styles["nav-container"]}`}
         >
-          <li>
-            <a href="#home">Categories</a>
-          </li>
-          <li>
-            <a className="btn btn-primary" href="/login">
-              Login
-            </a>
-          </li>
-        </ul>
+          <Logo />
 
-        <FontAwesomeIcon icon={faBars} onClick={handleMenuClick} />
+          {/* Dropdown menu */}
+          <ul
+            className={`d-flex list-unstyled gap-2 ${styles["nav-link-parent"]} ${styles["show"]}`}
+          >
+            <li>
+              <a href="#home">Categories</a>
+            </li>
+            <li className="ms-4">
+              <a
+                type="button"
+                className="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                Login
+              </a>
+            </li>
+          </ul>
+
+          <FontAwesomeIcon icon={faBars} onClick={handleMenuClick} />
+        </div>
+      </nav>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Login
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                  <div id="emailHelp" className="form-text">
+                    We will never share your email with anyone else.
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <div className="mb-3 form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label" htmlFor="exampleCheck1">
+                    Check me out
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-primary w-100 mb-4">
+                  Login
+                </button>
+
+                <hr />
+
+                <div className="text-center d-flex justify-content-center gap-1">
+                  <p>Not a member yet?</p>
+                  <a
+                    href="#"
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#registerModal"
+                  >
+                    Register
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    </nav>
+
+      <div
+        className="modal fade"
+        id="registerModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Register
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="firstName" className="form-label">
+                    First Name
+                  </label>
+                  <input type="text" className="form-control" id="firstName" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="lastName" className="form-label">
+                    Last Name
+                  </label>
+                  <input type="text" className="form-control" id="lastName" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="email" className="form-label">
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    aria-describedby="emailHelp"
+                  />
+                  <div id="emailHelp" className="form-text">
+                    We will never share your email with anyone else.
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="birthdate" className="form-label">
+                    Birthdate
+                  </label>
+                  <input type="date" className="form-control" id="birthdate" />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary w-100 mb-4">
+                  Register
+                </button>
+
+                <hr />
+
+                <div className="text-center d-flex justify-content-center gap-1">
+                  <p>Already a member?</p>
+                  <a
+                    href="#"
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                  >
+                    Login
+                  </a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
