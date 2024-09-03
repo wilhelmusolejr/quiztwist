0ccb3ebc91ba98d27fcd9472102cd1c8a1fea206
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-09-03 17:53:06 +0800

    backend test 1

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import quizRoutes from "./routes/quiz.route.js";
 import { connectDB } from "./db/connectDB.js";
 import cors from "cors";
 
+import { Question } from "./models/question.model.js";
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -22,11 +24,47 @@ app.get("/api", (req, res) => {
   res.send("Hello World");
 });
 
+export const getListQuestions = async (req, res) => {
+  const { number_question, category } = req.body;
+
+  try {
+    const allQuestions = await Question.find({ category });
+    const shuffledQuestions = allQuestions.sort(() => 0.5 - Math.random());
+    const selectedQuestions = shuffledQuestions.slice(
+      0,
+      parseInt(number_question)
+    );
+    return res
+      .status(200)
+      .json({ success: true, questions: selectedQuestions });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+app.get("/api/question", async (req, res) => {
+  const { number_question, category } = req.body;
+
+  try {
+    const allQuestions = await Question.find({ category });
+    const shuffledQuestions = allQuestions.sort(() => 0.5 - Math.random());
+    const selectedQuestions = shuffledQuestions.slice(
+      0,
+      parseInt(number_question)
+    );
+    return res
+      .status(200)
+      .json({ success: true, questions: selectedQuestions });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+});
+
 // ROUTE
 app.use("/api/auth", authRoutes);
 
 // QUESTION
-app.use("/api/question", questionRoutes);
+// app.use("/api/question", questionRoutes);
 
 // QUIZ
 app.use("/api/quiz", quizRoutes);
