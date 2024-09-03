7e549d61ff558253896356187fde37618e6c36a3
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-09-03 10:57:34 +0800

    link refractor

--- frontend/src/Components/CategoryContainer.jsx
+++ frontend/src/Components/CategoryContainer.jsx
@@ -1,16 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import styles from "./CategoryContainer.module.css";
+import { useLocation } from "react-router-dom";
 
 export default function CategoryContainer({ icon, title = "", link = "#" }) {
+  const location = useLocation();
+
   return (
-    <a
-      href={link}
-      className={`${styles["child"]} rounded d-flex flex-column gap-3
-     justify-content-center align-items-center text-light`}
-    >
-      <FontAwesomeIcon icon={icon} className={`${styles["svg"]}`} />
-      <p>{title}</p>
-    </a>
+    <>
+      {location.pathname === "/" ? (
+        <a
+          href={link}
+          className={`${styles.child} rounded d-flex flex-column gap-3
+           justify-content-center align-items-center text-light`}
+        >
+          <FontAwesomeIcon icon={icon} className={styles.svg} />
+          <p>{title}</p>
+        </a>
+      ) : (
+        <div
+          className={`${styles.child} rounded d-flex flex-column gap-3
+           justify-content-center align-items-center text-light cursor-default`}
+        >
+          <FontAwesomeIcon icon={icon} className={styles.svg} />
+          <p>{title}</p>
+        </div>
+      )}
+    </>
   );
 }
