42adfff994443e12cd2e399eb0445e4ff6e3b977
Wilhelmus Ole Jr <wilhelmus.olejr@gmail.com>
2024-09-03 15:31:15 +0800

    update cors

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Use CORS middleware before defining routes
-app.use(
-  cors({
-    origin: "https://quiztwist-frontend.vercel.app", // Allow only this origin
-  })
-);
-
+app.use(cors());
 app.use(express.json()); // Allows us to parse JSON data in the body of the request
 
 app.get("/", (req, res) => {
